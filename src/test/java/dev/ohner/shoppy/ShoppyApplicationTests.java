07c6ed47b05ecf138fd3a8625e7ba451f9e08310
gernot-ohner <gernot.ohner@gmail.com>
2021-01-07 12:42:06 +0100

    Initial commit

---
+++ src/test/java/dev/ohner/shoppy/ShoppyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ohner.shoppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
